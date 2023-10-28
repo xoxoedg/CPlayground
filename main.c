751d342e5004273e6d9fcdfb28f9ae2cbee3f699
xoxoedg <rugyasin@gmail.com>
2023-10-28 16:25:21 +0200

    Stuff

--- main.c
+++ main.c
@@ -1,16 +1,48 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+struct customer {
+    char vname[20];
+    char nname[20];
+    long PLZ;
+    char ort[20];
+    int geburtsjahr;
+};
+
+
+struct customer changeName(struct customer custom, char name[]) {
+    strcpy(custom.nname, name);
+    return custom;
+}
+
+void printCust(struct customer custom) {
+    printf("%s\n", custom.nname);
+    printf("%s\n", custom.vname);
+    printf("%li\n", custom.PLZ);
+    printf("%i\n", custom.geburtsjahr);
+    printf("%s\n", custom.ort);
+}
+
 
 int main(void) {
-    int wert=10;
-    int *ptr1;
+    struct customer willi = {
+            "Werner", "Walter", 80937, "Frankreich", 10101996
+    };
 
-    ptr1 = &wert;
+    printCust(willi);
+
+    printf("-----------------------\n");
+
+    struct customer walter = changeName(willi, "Brumm");
+    printCust(walter);
 
 
-    printf("ptr1 verweist auf %p\n", ptr1);
-    printf("Wert in %p ist %d\n\n", ptr1, *ptr1);
-    printf("Adresse von ptr1 : %p\n", &ptr1);
-
-    return EXIT_SUCCESS;
+    return 0;
 }
+
+
+
+
+
