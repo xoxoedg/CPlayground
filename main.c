73fe99c4f8a6b9cb78b9904410e09b7c78fa7f29
xoxoedg <rugyasin@gmail.com>
2023-10-28 14:02:28 +0200

    Playing with realloc

--- main.c
+++ main.c
@@ -1,16 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    int wert=10;
-    int *ptr1;
+/*int main(void) {
+    char *arr;
+    int size = 0, i=0;
+    arr = malloc(size*sizeof(char));
+    if (arr == NULL) {
+        printf("Kein Speicherplatz gefunden");
+        return EXIT_FAILURE;
+    }
 
-    ptr1 = &wert;
+    printf("Wie viele werte sollen in das array geschrieben werden\n");
+    scanf("%d", &size);
 
+    while (i < size) {
+        printf("geben sie die werte der arrays an: \n");
+        scanf("%s", (arr + i));
+        i++;
+    }
 
-    printf("ptr1 verweist auf %p\n", ptr1);
-    printf("Wert in %p ist %d\n\n", ptr1, *ptr1);
-    printf("Adresse von ptr1 : %p\n", &ptr1);
+    for (int j = 0; j < size; j++) {
+        printf("%c", *(arr + j));
+    }
+
 
     return EXIT_SUCCESS;
+}*/
+
+
+int main() {
+    int size = 0;
+    int i = 0;
+    printf("How many memory you need: \n");
+    scanf("%d", &size);
+
+    int *inArray = malloc(size * sizeof(int));
+
+    while (i < size) {
+        printf("Geben sie  die einzelnen Elemente an\n");
+        scanf("%d", (inArray + i));
+        i++;
+    }
+
+    int resize = 0;
+    printf("Wie viel platz brauchen sie mehr: \n");
+    scanf("%d", &resize);
+
+    inArray = realloc(inArray, (resize + size) * sizeof(int));
+
+    int finalSize = size + resize;
+    while (i < (finalSize)) {
+        inArray = realloc(inArray, (finalSize) * sizeof(int));
+        printf("Geben sie  die einzelnen Elemente an\n");
+        scanf("%d", (inArray + i));
+        i++;
+    }
+
+    for (int j = 0; j < resize + size; j++) {
+        printf("%d\n", *(inArray + j));
+    }
+
+
+    return 0;
 }
