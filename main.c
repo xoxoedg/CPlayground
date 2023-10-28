013b0cee03331112f741294e312139f5cf9022b1
xoxoedg <rugyasin@gmail.com>
2023-10-28 21:04:45 +0200

    Reset master branch

--- main.c
+++ main.c
@@ -2,15 +2,5 @@
 #include <stdlib.h>
 
 int main(void) {
-    int wert=10;
-    int *ptr1;
 
-    ptr1 = &wert;
-
-
-    printf("ptr1 verweist auf %p\n", ptr1);
-    printf("Wert in %p ist %d\n\n", ptr1, *ptr1);
-    printf("Adresse von ptr1 : %p\n", &ptr1);
-
-    return EXIT_SUCCESS;
 }
